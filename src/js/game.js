0476a67ebf907a198c074e7100c45186b474e4aa
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-09-17 03:08:33 +0300

    complete fix

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,16 @@
-class Game {
+import Character from './domain.js';
+
+export default class Game {
   start() {
     console.log('game started');
   }
 }
 
-class GameSavingData {
+export class GameSavingData {
 }
 
-function readGameSaving() {
+export function readGameSaving() {
 }
 
-function writeGameSaving() {
+export function writeGameSaving() {
 }
